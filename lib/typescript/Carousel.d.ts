e040ea5fffdf99a0ecae3a15c23d6238bdf8afeb
bokuzminski <cookies765@gmail.com>
2022-01-14 12:33:31 +0100

    reverted gitignore change

--- lib/typescript/Carousel.d.ts
+++
@@ -1,5 +0,0 @@
-import React, { PropsWithChildren } from 'react';
-import type { ICarouselInstance, TCarouselProps } from './types';
-declare function Carousel<T>(_props: PropsWithChildren<TCarouselProps<T>>, ref: React.Ref<ICarouselInstance>): JSX.Element;
-declare const _default: typeof Carousel;
-export default _default;
--- lib/typescript/constants/index.d.ts
+++
@@ -1,7 +0,0 @@
-export declare enum DATA_LENGTH {
-    SINGLE_ITEM = 1,
-    DOUBLE_ITEM = 2
-}
-export declare const Easing: {
-    easeOutQuart: (value: number) => number;
-};
